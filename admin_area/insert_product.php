b6a1a8b43f4bc0cf4eff0f1b7eac4070a1a6039e
sahan1416375 <sahanhasintha12345@gmail.com>
2019-07-04 23:08:46 +0530

    Second update here

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -2,31 +2,32 @@
 <!DOCTYPE html>
 <?php
 
-include("includes/db.php")
+include("includes/db.php");
 
 ?>
 <html>
 <head>
 	<title>Inserting Product</title>
-	<script type="text/javascript" src="<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+	<script type="text/javascript"  src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 	<script>
 		tinymce.init({selector:textarea});
 	</script>
 </head>
 <body bgcolor="skyblue">
+	<form action="insert_product.php" method="post" enctype="multipart/form-data">
 	<table align="center" border="2" width="600px"bgcolor="orange">
 		<tr align="center">
 			<td colspan="6"><h1>Insert new post here</h1></td>
 		</tr>
 		<tr>
 			<td align="right"><b>Product Title : </b></td>
-			<td><input type="text" name="product_title" size="50"></td>
+			<td><input type="text" name="product_title" size="50"/></td>
 		</tr>
 
 		<tr>
 			<td align="right"><b>Product Category : </b></td>
 			<td>
-				<select name="product_cat">
+				<select name="product_cat"/>
 					<option>Select a Category</option>
 					<?php
 					$get_cats="select * from categories";
@@ -48,7 +49,7 @@ include("includes/db.php")
 		<tr>
 			<td align="right"><b>Product Brand : </b></td>
 			<td>
-				<select name="product_brand">
+				<select name="product_brand"/>
 					<option>Select a Brand</option>
 				<?php $get_brand="select * from brands";
 	$run_brand=mysqli_query($con, $get_brand);
@@ -69,7 +70,7 @@ include("includes/db.php")
 
 		<tr>
 			<td align="right"><b>Product Image : </b></td>
-			<td><input type="file" name="product_image"></td>
+			<td><input type="file" name="product_image"/></td>
 		</tr>
 
 		<tr>
@@ -84,13 +85,47 @@ include("includes/db.php")
 
 		<tr>
 			<td align="right"><b>Product Keywords : </b></td>
-			<td><input type="text" name="product_title" size="50"></td>
+			<td><input type="text" name="product_keywords" size="50"></td>
 		</tr>
 
 		<tr align="center">
 			<td colspan="6"><input type="submit" name="insert_post" value="Insert product Now"/></td>
 		</tr>
 	</table>
-
+</form>
 </body>
 </html>
+
+<?php
+
+	$con=mysqli_connect("localhost","root","","e-commerce");
+	if (isset($_POST['insert_post'])) {
+		# code...
+		$product_title=$_POST['product_title'];
+		$product_cat=$_POST['product_cat'];
+		$product_brand=$_POST['product_brand'];
+		$product_price=$_POST['product_price'];
+
+		$product_image=$_FILES['product_image']['name'];
+		$product_image_tmp=$_FILES['product_image']['tmp_name'];
+
+		$product_desc=$_POST['product_desc'];
+		$product_keywords=$_POST['product_keywords'];
+ 
+ 		move_uploaded_file($product_image_tmp, "images/$product_image");
+
+		 $insert_product="INSERT INTO products (product_cat, product_brand, product_title, product_price, product_desc, product_image, product_keywords)  VALUES ('$product_cat','$product_brand','$product_title','$product_price','$product_desc','$product_image',$product_keywords')";
+
+		$insert_pro =mysqli_query($con, $insert_product);
+
+		if($insert_pro){
+		echo "<script>alert('Product has been inserted')</script>";
+		#echo "<script>window.open('insert_product.php','_self')</script>";
+
+		}
+		else{
+			echo "<script>alert('Wrong')</script>";
+		}
+    }
+	
+?>
